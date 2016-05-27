da677e34cb7c8c7f3feb699e1d3dcfbbaf2fd5ee
David Faulkenberry <david.faulkenberry@gmail.com>
2016-05-27 16:48:40 -0400

    Update logic

--- app/controllers/bus_stops_controller.rb
+++ app/controllers/bus_stops_controller.rb
@@ -1,11 +1,21 @@
 class BusStopsController < ApplicationController
-  before_action :find_stop, only: %i(edit)
+  before_action :find_stop, only: %i(edit update)
 
   def edit
   end
 
   def index
     @stops = BusStop.all
+  end
+
+  def update
+    if @stop.update stop_params
+      flash[:notice] = 'Bus stop was updated.'
+      redirect_to bus_stops_path
+    else
+      flash[:errors] = @stop.errors.full_messages
+      redirect_to :back
+    end
   end
 
   private
@@ -15,5 +25,10 @@ class BusStopsController < ApplicationController
     unless @stop.present?
       render nothing: true, status: :not_found and return
     end
+  end
+
+  def stop_params
+    # no attributes that people aren't supposed to be able to edit
+    params.require(:bus_stop).permit!
   end
 end
