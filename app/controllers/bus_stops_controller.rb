150856af565b36114efcfab0c6e7e15a4a144290
David Faulkenberry <david.faulkenberry@gmail.com>
2016-05-27 16:42:17 -0400

    Edit form

--- app/controllers/bus_stops_controller.rb
+++ app/controllers/bus_stops_controller.rb
@@ -1,5 +1,19 @@
 class BusStopsController < ApplicationController
+  before_action :find_stop, only: %i(edit)
+
+  def edit
+  end
+
   def index
     @stops = BusStop.all
+  end
+
+  private
+
+  def find_stop
+    @stop = BusStop.find_by id: params.require(:id)
+    unless @stop.present?
+      render nothing: true, status: :not_found and return
+    end
   end
 end
