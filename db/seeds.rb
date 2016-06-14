b823bff961da15afb097cfe321b6ac723d196618
David Faulkenberry <david.faulkenberry@gmail.com>
2016-06-14 17:58:45 -0400

    Some more extensive seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,28 +4,46 @@ User.create! name: 'David Faulkenberry',
              password_confirmation: 'password',
              admin: true
 
-User.create! name: 'George Boole',
-             email: 'george@example.com',
+User.create! name: 'Jake Foreman',
+             email: 'jake@example.com',
              password: 'password',
              password_confirmation: 'password',
              admin: false
 
-Route.create! number: '30'
-sasn = Route.create! number: '31'
+routes = {
+  30 => Route.create!(number: '30'),
+  31 => Route.create!(number: '31'),
+  38 => Route.create!(number: '38')
+}
 
-BusStop.create! name: 'Studio Arts Building',
-                hastus_id: 72,
-                routes: [sasn],
-                accessible: 'When necessary',
-                bench: 'PVTA',
-                curb_cut: "Within 20'",
-                lighting: "50\" - 20'",
-                mounting: 'PVTA pole',
-                mounting_direction: 'Towards street',
-                shelter: 'PVTA',
-                sidewalk: 'More than 36"',
-                sign: nil,
-                trash: 'Other',
-                bus_pull_out_exists: true,
-                has_power: true,
-                system_map_exists: false
+stops = {
+  30 => ['Puffton Village', 'GRC', 'Cowles Ln', 'Colonial Village', 'Old Belchertown Rd', 'Amherst Post Office', 'Studio Arts Building'],
+  31 => ['Sugarloaf Estates', 'Townhouse Apts', 'GRC', 'Cowles Ln', 'The Boulders', 'Amherst Post Office', 'Studio Arts Building'],
+  38 => ['Haigis Mall', 'Cowles Ln', 'Amherst College', 'Hampshire College', 'Blanchard Hall']
+}
+
+hastus_ids = {
+  'Amherst College'      => 116,
+  'Amherst Post Office'  => 96,
+  'Blanchard Hall'       => 336,
+  'Colonial Village'     => 120,
+  'Cowles Ln'            => 95,
+  'GRC'                  => 58,
+  'Haigis Mall'          => 73,
+  'Hampshire College'    => 236,
+  'Old Belchertown Rd'   => 148,
+  'Puffton Village'      => 34,
+  'Studio Arts Building' => 72,
+  'Sugarloaf Estates'    => 9,
+  'The Boulders'         => 157,
+  'Townhouse Apts'       => 30
+}
+
+stops.each do |route_number, stop_names|
+  stop_names.each do |stop_name|
+    stop = BusStop.find_or_initialize_by name: stop_name
+    stop.hastus_id = hastus_ids.fetch(stop_name)
+    stop.routes << routes.fetch(route_number)
+    stop.save!
+  end
+end
