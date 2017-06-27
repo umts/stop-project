3413a3950164cf92b48d826854ca802579f935c8
Matt Moretti <werebus@gmail.com>
2017-06-27 11:27:36 -0400

    Upgraded capistrano to 3.8.1

--- Capfile
+++ Capfile
@@ -1,3 +1,7 @@
-%w(setup deploy pending bundler rails passenger)
-  .each { |r| require "capistrano/#{r}" }
+%w[setup deploy scm/git pending bundler rails passenger].each do |lib|
+  require "capistrano/#{lib}"
+end
+
+install_plugin Capistrano::SCM::Git
+
 Dir.glob('lib/capistrano/tasks/*.rake').each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,18 +1,12 @@
 # config valid only for current version of Capistrano
-lock '3.5.0'
+lock '3.8.1'
 
 set :application, 'stop-project'
 set :repo_url, 'git@github.com:umts/stop-project.git'
 set :branch, :master
 set :deploy_to, "/srv/#{fetch :application}"
 set :log_level, :info
-set :scm, :git
 set :keep_releases, 5
 
-set :linked_files, fetch(:linked_files, []).push(
-  'config/database.yml'
-)
-
-set :linked_dirs, fetch(:linked_dirs, []).push(
-  'log'
-)
+set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :linked_dirs, fetch(:linked_dirs, []).push('log')
