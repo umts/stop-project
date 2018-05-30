08db1fa97d968fc7c65f52abe5a189e68914185c
Molly O'Neil <mboneil@umass.edu>
2018-05-30 16:31:34 -0400

    Ternaries suck

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,9 @@
 module ApplicationHelper
 
-  def display_name(stop)
-    if stop.name.present? then stop.name
-    else 'New stop'
+  def display_name(item)
+    case item
+    when :user then item.name.present? ? item.name : 'New user'
+    when :stop then item.name.present? ? item.name : 'New stop'
     end
   end
 end
