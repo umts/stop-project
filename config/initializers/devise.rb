9916ba0ddfa1f0dcb8e0e15feec00a454b567789
Molly O'Neil <mboneil@umass.edu>
2018-05-31 16:19:57 -0400

    remove mail sender from config devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,7 +1,6 @@
 Devise.setup do |config|
   require 'devise/orm/active_record'
   
-  config.mailer_sender = 'stop-project@pvtaapps.com'
   config.case_insensitive_keys = [:email]
   config.strip_whitespace_keys = [:email]
   config.stretches = Rails.env.test? ? 1 : 10
