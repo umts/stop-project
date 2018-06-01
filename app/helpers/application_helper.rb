ae94663c8265cd0e438df9c683b33c34c37b8a22
David Faulkenberry <dfaulken@umass.edu>
2018-06-01 14:03:14 -0400

    Goodbye ternaries

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,9 +3,9 @@ module ApplicationHelper
   def display_name(item)
     case item
     when User
-      item.name.present? ? item.name : 'New user'
+      item.name || 'New user'
     when BusStop
-      item.name.present? ? item.name : 'New stop'
+      item.name || 'New stop'
     end
   end
 end
