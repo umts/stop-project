fa6d2b5293ff5cd6b023d4f2e1370157e10d8e42
Molly O'Neil <mboneil@umass.edu>
2018-06-01 11:25:40 -0400

    I hate ternaries and everything about this method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
 
   def display_name(item)
-    case item
-    when :user then item.name.present? ? item.name : 'New user'
-    when :stop then item.name.present? ? item.name : 'New stop'
+    case item.class
+    when User then item.name.present? ? item.name : 'New user'
+    when BusStop then item.name.present? ? item.name : 'New stop'
     end
   end
 end
