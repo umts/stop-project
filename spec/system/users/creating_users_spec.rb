2c68a94a487812e13b9f4de17c4c171f3dd44d45
Molly O'Neil <mboneil@umass.edu>
2018-06-03 19:49:44 -0400

    Skeleton for new users page spec

---
+++ spec/system/users/creating_users_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe 'creating users' do
+  context 'as an admin' do
+    it 'creates a user' do
+    end
+    it 'notifies you the user has been created' do
+    end
+    it 'redirects you to the users page' do
+    end
+  end
+  context 'as a regular user' do
+  end
+end
