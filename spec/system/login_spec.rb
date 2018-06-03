4758d635a0917237a2f5115ca06df8003900f043
Molly O'Neil <mboneil@umass.edu>
2018-06-03 12:34:46 -0400

    spec from the internet was a bad choice

--- spec/system/login_spec.rb
+++ spec/system/login_spec.rb
@@ -1,10 +1,6 @@
 require 'spec_helper'
 
 describe 'Logging In' do
-  before do
-    driven_by :selenium_chrome_headless
-  end
-  
   it 'shows the right page' do
     visit root_url
     expect(page).to have_content 'Hello World'
