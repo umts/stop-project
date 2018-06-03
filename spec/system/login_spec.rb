8e3735fde30a2bc24e717d2e470f9430cb940890
Molly O'Neil <mboneil@umass.edu>
2018-06-03 12:32:19 -0400

    Make system directory and failing test

---
+++ spec/system/login_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe 'Logging In' do
+  before do
+    driven_by :selenium_chrome_headless
+  end
+  
+  it 'shows the right page' do
+    visit root_url
+    expect(page).to have_content 'Hello World'
+  end
+end
