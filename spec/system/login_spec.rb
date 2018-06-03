708ccc421ce23f0460b1a7a511216b39c3d6977a
Molly O'Neil <mboneil@umass.edu>
2018-06-03 13:02:11 -0400

    Skeleton for the actual site

--- spec/system/login_spec.rb
+++ spec/system/login_spec.rb
@@ -1,8 +1,24 @@
 require 'spec_helper'
 
-describe 'Logging In' do
-  it 'shows the right page' do
-    visit root_url
-    expect(page).to have_content 'Hello World'
+describe 'logging in as a user' do
+  context 'correct email and password' do
+    it 'sends current user to the index' do
+    end
+    it 'flashes a success message' do
+    end
+  end
+  context 'incorrect email or password' do
+    it "doesn't send current user to the index" do
+    end
+  end
+  it 'displays a login notice' do
+  end
+end
+
+# I don't think this is necessary. We should trust devise.
+describe 'logging out as a user' do
+  it 'logs current user out' do
+  end
+  it 'redirects to login page' do
   end
 end
