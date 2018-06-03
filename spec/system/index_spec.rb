be555a85e658468a080437efcddbe76b67b9eceb
Molly O'Neil <mboneil@umass.edu>
2018-06-03 14:19:54 -0400

    Skeleton for index sepc

---
+++ spec/system/index_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe 'searching for a bus stop by stop id' do
+  context 'correct stop id' do
+    it 'redirects to the edit page' do
+    end
+  end
+   context 'incorrect stop id' do
+     it 'stays on the same page' do
+     end
+     it 'displays a helpful message' do
+     end
+   end
+end
+
+describe 'searching for a bus stop by stop name' do
+  context 'correct stop name' do
+    it 'redirects to the edit page' do
+    end
+  end
+  context 'incorrect stop name' do
+    it 'stays on the same page' do
+    end
+    it 'displays a helpful message' do
+    end
+  end
+end
+
+describe 'searching for a bus stop by route' do
+  context 'route from the dropdown' do
+    it 'redirects to bus stops by route page' do
+    end
+  end
+end
+
