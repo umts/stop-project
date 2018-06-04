7dbb4d8275fe88a34d7f784b69b1df777d001d04
Molly O'Neil <mboneil@umass.edu>
2018-06-03 23:31:03 -0400

    file for editing users test

---
+++ spec/system/users/editing_users_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe 'editing a user as an admin' do
+  before :each do
+    admin = create :user, :admin
+    edit_user = create :user
+    
+    when_current_user_is admin
+    visit edit_user, id: edit_user.id
+  end
+  context 'edit a user' do
+  end
+end
