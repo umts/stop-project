634439f4196fda16b74987e69e2d84741a894d84
Molly O'Neil <mboneil@umass.edu>
2018-06-04 15:18:51 -0400

    Update a user without changes

--- spec/system/users/editing_users_spec.rb
+++ spec/system/users/editing_users_spec.rb
@@ -1,10 +1,25 @@
 require 'spec_helper'
 
 describe 'editing a user as an admin' do
+  before :each do
+    admin = create :user, :admin
+    @edit_user = create :user
+    
+    when_current_user_is admin
+    visit edit_user_path(@edit_user)
+  end
   context 'with no changes' do
+    before :each do
+      within 'table.edit-form' do
+        click_button 'Save user'
+      end
+    end
     it 'updates the user' do
+      expect(page).to have_selector 'p.notice',
+        text: 'User was updated.'
     end
     it 'redirect to users table' do
+      expect(page.current_path).to end_with users_path
     end
   end
   context 'with name, email, or admin changes' do
