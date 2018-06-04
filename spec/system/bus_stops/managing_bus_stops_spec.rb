1d00fcb426dd1ab6cc54a2397878ebe4d7d61126
Molly O'Neil <mboneil@umass.edu>
2018-06-04 15:47:15 -0400

    calling .to_s on string. How could I have been so careless

--- spec/system/bus_stops/managing_bus_stops_spec.rb
+++ spec/system/bus_stops/managing_bus_stops_spec.rb
@@ -12,19 +12,19 @@ describe 'managing stops as an admin' do
   context 'delete button' do
     it 'deletes the specific bus stop' do
       expect(page).to have_selector 'table.manage tbody tr', count: 1
-      within 'tr', text: @bus_stop.name.to_s do
+      within 'tr', text: @bus_stop.name do
         click_button 'Delete'
       end
       page.driver.browser.switch_to.alert.accept
       expect(page).to have_selector 'p.notice',
         text: "#{@bus_stop.name} has been deleted."
-      expect(page).not_to have_selector 'table.manage tbody tr', text: @bus_stop.name.to_s
+      expect(page).not_to have_selector 'table.manage tbody tr', text: @bus_stop.name
     end
   end
   context 'edit button' do
     it 'redirects to edit bus stop page' do
       expect(page).to have_selector 'table.manage tbody tr', count: 1
-      within 'tr', text: @bus_stop.name.to_s do
+      within 'tr', text: @bus_stop.name do
         click_link 'Edit'
       end
       expect(page).to have_content "Editing #{@bus_stop.name}"
