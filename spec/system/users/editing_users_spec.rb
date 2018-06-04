6bb1799f3bf3e317986336de795f1f0ea4988576
Molly O'Neil <mboneil@umass.edu>
2018-06-04 18:03:02 -0400

    Some more tests for errors

--- spec/system/users/editing_users_spec.rb
+++ spec/system/users/editing_users_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe 'editing a user as an admin' do
   before :each do
-    admin = create :user, :admin
+    @admin = create :user, :admin
     @edit_user = create :user
     
-    when_current_user_is admin
+    when_current_user_is @admin
     visit edit_user_path(@edit_user)
   end
   context 'with no changes' do
@@ -42,6 +42,21 @@ describe 'editing a user as an admin' do
       expect(page).not_to have_css 'td', text: @previous_name
       expect(page).not_to have_css 'td', text: @previous_email
       expect(page).not_to have_css 'span', class: 'glyphicon glyphicon-remove'
+    end
+  end
+  context 'with errors' do
+    before :each do
+      within 'table.edit-form' do
+        fill_in 'Name', with: @admin.name
+        click_button 'Save user'
+      end
+    end
+    it 'sends a helpful error message' do
+      expect(page).to have_selector 'p.errors',
+        text: 'Name has already been taken'
+    end
+    it 'stays on the edit page' do
+      expect(page).to have_content "Editing #{@edit_user.name}"
     end
   end
 end
