7ba8333b52f17acd8b3276472cf7ec1a1924694e
Molly O'Neil <mboneil@umass.edu>
2018-06-04 14:16:50 -0400

    Nothing like starting from scratch. Without git I guess

--- spec/system/index_spec.rb
+++ spec/system/index_spec.rb
@@ -1,33 +1,14 @@
 require 'spec_helper'
 
 describe 'searching for a bus stop by stop id' do
-  before :each do
-    user = create :user
-    when_current_user_is user
-    visit root_url
-    @bus_stop = create :bus_stop
-  end
   context 'correct stop id' do
     it 'redirects to the edit page' do
-      within 'form', text: 'Enter stop ID' do
-        fill_in 'id', with: @bus_stop.id
-        click_button 'Search'
-      end
     end
   end
    context 'incorrect stop id' do
-     before :each do
-       within 'form', text: 'Enter stop ID' do
-         fill_in 'id', with: -@bus_stop.id
-         click_button 'Search'
-       end
-     end
      it 'stays on the same page' do
-       expect(page.current_url).to match root_path
      end
      it 'displays a helpful message' do
-       expect(page).to have_selector 'p.notice',
-         text: "Stop #{-@bus_stop.id} not found"
      end
    end
 end
