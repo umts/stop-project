e3a94235ec2a6b60e7fb1cb09a321d6756e668d9
Molly O'Neil <mboneil@umass.edu>
2018-06-03 21:18:30 -0400

    Skeleton for managing users

---
+++ spec/system/users/managing_users_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe 'managing users as an admin' do
+  context 'delete button' do
+    it 'deletes the specific user' do
+    end
+  end
+  context 'edit button' do
+    it 'redirects to edit user page' do
+    end
+  end
+end
