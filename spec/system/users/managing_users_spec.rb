6e6b76c6c9b2de964e77ea43cf60eb25f156ce63
Molly O'Neil <mboneil@umass.edu>
2018-06-03 22:11:14 -0400

    Some tests for manage users page

--- spec/system/users/managing_users_spec.rb
+++ spec/system/users/managing_users_spec.rb
@@ -1,12 +1,32 @@
 require 'spec_helper'
 
 describe 'managing users as an admin' do
+  before :each do
+    admin = create :user, :admin, name: 'Admin'
+    delete_user = create :user, name: 'Jane'
+    
+    when_current_user_is admin
+    visit users_url
+
+  end
   context 'delete button' do
     it 'deletes the specific user' do
+      expect(page).to have_selector 'table.index tbody tr', count: 2
+      within 'tr', text: 'Jane' do
+        click_button 'Delete'
+      end
+      expect(page).to have_selector 'p.notice',
+        text: 'User was deleted.'
+      expect(page).not_to have_selector 'table.index tbody tr', text: 'Jane'
     end
   end
   context 'edit button' do
     it 'redirects to edit user page' do
+      expect(page).to have_selector 'table.index tbody tr', count: 2
+      within 'tr', text: 'Jane' do
+        click_button 'Edit'
+      end
+      expect(page).to have_content 'Editing Jane'
     end
   end
 end
