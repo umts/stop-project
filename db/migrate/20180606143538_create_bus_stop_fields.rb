4d0eea10a951e533ba72fa9138ac887dd6e86256
Molly O'Neil <mboneil@umass.edu>
2018-06-06 11:44:41 -0400

    Joins table instead

--- db/migrate/20180606143538_create_bus_stop_fields.rb
+++ db/migrate/20180606143538_create_bus_stop_fields.rb
@@ -1,11 +1,10 @@
 class CreateBusStopFields < ActiveRecord::Migration[5.1]
   def change
     create_table :bus_stop_fields do |t|
-      t.integer :bus_stop_id
-      t.string :field_name
       t.string :value
 
       t.timestamps
     end
+    create_join_table :bus_stops, :field_names
   end
 end
