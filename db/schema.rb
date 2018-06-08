d6eddfb39eb625be87d403e6c5a16141df695ab0
Molly O'Neil <mboneil@umass.edu>
2018-06-08 15:17:39 -0400

    Choices a part of rails now

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180606143538) do
+ActiveRecord::Schema.define(version: 20180608191700) do
 
   create_table "bus_stop_fields", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "value"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 20180606143538) do
     t.string "field_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "choices"
   end
 
   create_table "routes", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
