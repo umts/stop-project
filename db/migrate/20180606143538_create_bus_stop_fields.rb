40194a22e5d5cf3b30cfad714215de50d70974fc
Molly O'Neil <mboneil@umass.edu>
2018-06-09 09:42:20 -0400

    Table name changed

--- db/migrate/20180606143538_create_bus_stop_fields.rb
+++ db/migrate/20180606143538_create_bus_stop_fields.rb
@@ -1,6 +1,6 @@
 class CreateBusStopFields < ActiveRecord::Migration[5.1]
   def change
-    create_table :bus_stop_fields do |t|
+    create_join_table :bus_stops, :fields, table_name: :bus_stop_fields do |t|
       t.string :value
 
       t.timestamps
