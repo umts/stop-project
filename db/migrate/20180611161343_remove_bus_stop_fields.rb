9511e8e6c3d8d4d30503d2acdc626b1d56e65934
Justin <juforgue@gmail.com>
2018-06-12 10:39:41 -0400

    make migration reversable

--- db/migrate/20180611161343_remove_bus_stop_fields.rb
+++ db/migrate/20180611161343_remove_bus_stop_fields.rb
@@ -1,11 +1,11 @@
 class RemoveBusStopFields < ActiveRecord::Migration[5.1]
   def change
-    remove_column :bus_stops, :sign
-    remove_column :bus_stops, :extend_pole
-    remove_column :bus_stops, :new_anchor
-    remove_column :bus_stops, :new_pole
-    remove_column :bus_stops, :straighten_pole
-    remove_column :bus_stops, :mounting_clearance_after
-    remove_column :bus_stops, :accessible
+    remove_column :bus_stops, :sign, :string
+    remove_column :bus_stops, :extend_pole, :boolean
+    remove_column :bus_stops, :new_anchor, :boolean
+    remove_column :bus_stops, :new_pole, :boolean
+    remove_column :bus_stops, :straighten_pole, :boolean
+    remove_column :bus_stops, :mounting_clearance_after, :integer
+    remove_column :bus_stops, :accessible, :string
   end
 end
