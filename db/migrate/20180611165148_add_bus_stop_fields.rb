e24278e6101708ee919e57218deb396fc534b914
Justin <juforgue@gmail.com>
2018-06-12 10:14:50 -0400

    Remove date_stop_checked and stop_checked_by

--- db/migrate/20180611165148_add_bus_stop_fields.rb
+++ db/migrate/20180611165148_add_bus_stop_fields.rb
@@ -3,8 +3,6 @@ class AddBusStopFields < ActiveRecord::Migration[5.1]
     add_column :bus_stops, :shared_sign_post, :string
     add_column :bus_stops, :shelter_ada_compliance, :boolean
     add_column :bus_stops, :garage_responsible, :string
-    add_column :bus_stops, :date_stop_checked, :date
-    add_column :bus_stops, :stop_checked_by, :string
     add_column :bus_stops, :bike_rack, :string
     add_column :bus_stops, :ada_landing_pad, :boolean
     add_column :bus_stops, :real_time_information, :string
