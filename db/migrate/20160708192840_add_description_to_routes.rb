2e845c6642e59ef9016768918dbedf5db2eb2370
David Faulkenberry <david.faulkenberry@gmail.com>
2018-06-15 09:38:52 -0400

    Remove merge conflict marker

--- db/migrate/20160708192840_add_description_to_routes.rb
+++ db/migrate/20160708192840_add_description_to_routes.rb
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-class AddDescriptionToRoutes < ActiveRecord::Migration[5.1]
-=======
 class AddDescriptionToRoutes < ActiveRecord::Migration[4.2]
->>>>>>> c9726e8808c30c9a3f35e81b393f06d088a33726
   def change
     add_column :routes, :description, :string
   end
