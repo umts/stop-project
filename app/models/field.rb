19514243d5f8f4a8fce567efd0f4df0844bfe961
David Faulkenberry <david.faulkenberry@gmail.com>
2018-06-15 09:48:45 -0400

    Field validations

--- app/models/field.rb
+++ app/models/field.rb
@@ -3,6 +3,15 @@ class Field < ApplicationRecord
 
   has_many :bus_stop_fields, foreign_key: :field_name
   has_many :bus_stops, through: :bus_stop_fields
-
+  
+  validates :category, presence: true
+  validates :rank, presence: true, uniqueness: { scope: :category }
+  validates :field_type, inclusion: { in: %i[boolean choice] }
+  
   serialize :choices, Array
+  validates :choices, presence: true, if: :multiple_choice?
+  
+  def multiple_choice?
+    field_type == :choice
+  end
 end
