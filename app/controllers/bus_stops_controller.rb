5a5b5de1f2248f53b80841172c594f58935faf84
Anbranin <keichelman@gmail.com>
2018-06-15 15:40:39 -0400

    controller has access to current_user

--- app/controllers/bus_stops_controller.rb
+++ app/controllers/bus_stops_controller.rb
@@ -64,6 +64,7 @@ class BusStopsController < ApplicationController
 
   def update
     @stop.assign_attributes stop_params
+    @stop.completed? ? @stop.completed_by = current_user : nil
     if @stop.save
       flash[:notice] = 'Bus stop was updated.'
       redirect_to bus_stops_path
