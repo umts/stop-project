61bccba6a7773ed911c2c38e7dc08a1dea7746b0
Molly O'Neil <mboneil@umass.edu>
2018-06-19 15:21:05 -0400

    Has many bus_stops_routes

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,6 +1,7 @@
 class Route < ApplicationRecord
   validates :number, presence: true, uniqueness: true
   validates :description, presence: true
+  has_many :bus_stops_routes
   has_many :bus_stops, through: :bus_stops_routes
   default_scope { order :number }
 end
