6cfad06e0494ec9149584bb438ceb101c5189fe9
Molly O'Neil <mboneil@umass.edu>
2018-06-20 13:48:12 -0400

    Wrote wrong sequence min, need it to be 1

--- app/models/bus_stops_route.rb
+++ app/models/bus_stops_route.rb
@@ -2,5 +2,5 @@ class BusStopsRoute < ApplicationRecord
   belongs_to :bus_stop
   belongs_to :route
 
-  validates :sequence, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :sequence, presence: true, numericality: { greater_than_or_equal_to: 1 }
 end
