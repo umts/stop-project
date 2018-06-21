18f2513803e2ba509db537fff0aff05c42be5bdc
Molly O'Neil <mboneil@umass.edu>
2018-06-21 14:02:08 -0400

    uniqueness scope

--- app/models/bus_stops_route.rb
+++ app/models/bus_stops_route.rb
@@ -4,4 +4,6 @@ class BusStopsRoute < ApplicationRecord
 
   validates :sequence, presence: true, numericality: { greater_than_or_equal_to: 1 }
   validates :direction, presence: true
+  validates_uniqueness_of :sequence, scope: [ :route, :direction ]
+  validates_uniqueness_of :bus_stop, scope: [ :route, :direction ]
 end
