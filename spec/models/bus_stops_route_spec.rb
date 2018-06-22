6f856a8f13dd704271c06a73b9acf9182b7f983f
Molly O'Neil <mboneil@umass.edu>
2018-06-22 12:51:03 -0400

    Run rubocop again

--- spec/models/bus_stops_route_spec.rb
+++ spec/models/bus_stops_route_spec.rb
@@ -7,27 +7,29 @@ describe BusStopsRoute do
     context 'same route and direction' do
       let(:route) { create :route }
       let(:stop_1) { create :bus_stop }
-      let(:valid_bsr) { create :bus_stops_route,
-                        route: route,
-                        bus_stop: stop_1 }
+      let(:valid_bsr) {
+        create :bus_stops_route,
+               route: route,
+               bus_stop: stop_1
+      }
       context 'same sequence' do
         it 'is not valid' do
           stop_2 = create :bus_stop
 
           invalid_bsr = build :bus_stops_route,
-                        route: valid_bsr.route,
-                        direction: valid_bsr.direction,
-                        sequence: valid_bsr.sequence,
-                        bus_stop: stop_2
+                              route: valid_bsr.route,
+                              direction: valid_bsr.direction,
+                              sequence: valid_bsr.sequence,
+                              bus_stop: stop_2
           expect(invalid_bsr).not_to be_valid
         end
       end
       context 'same bus_stop' do
         it 'is not valid' do
           invalid_bsr = build :bus_stops_route,
-                        route: valid_bsr.route,
-                        direction: valid_bsr.direction,
-                        bus_stop: valid_bsr.bus_stop
+                              route: valid_bsr.route,
+                              direction: valid_bsr.direction,
+                              bus_stop: valid_bsr.bus_stop
           expect(invalid_bsr).not_to be_valid
         end
       end
