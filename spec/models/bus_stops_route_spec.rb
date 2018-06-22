fa2db94ff7044e355bb810d72df1a494b30a5c3c
Molly O'Neil <mboneil@umass.edu>
2018-06-22 12:30:44 -0400

    Line length

--- spec/models/bus_stops_route_spec.rb
+++ spec/models/bus_stops_route_spec.rb
@@ -13,16 +13,28 @@ describe BusStopsRoute do
           stop_2 = create :bus_stop
           sequence = 1
 
-          create :bus_stops_route, route: route, bus_stop: stop_1, sequence: sequence, direction: direction
-          invalid_bsr = build :bus_stops_route, route: route, bus_stop: stop_2, sequence: sequence, direction: direction
+          create :bus_stops_route, route: route,
+                                   bus_stop: stop_1,
+                                   sequence: sequence,
+                                   direction: direction
+          invalid_bsr = build :bus_stops_route, route: route,
+                                                bus_stop: stop_2,
+                                                sequence: sequence,
+                                                direction: direction
 
           expect(invalid_bsr).not_to be_valid
         end
       end
       context 'same bus_stop' do
         it 'is not valid' do
-          create :bus_stops_route, route: route, bus_stop: stop_1, direction: direction
-          invalid_bsr = build :bus_stops_route, route: route, bus_stop: stop_1, direction: direction
+          create :bus_stops_route,
+                 route: route,
+                 bus_stop: stop_1,
+                 direction: direction
+          invalid_bsr = build :bus_stops_route,
+                              route: route,
+                              bus_stop: stop_1,
+                              direction: direction
 
           expect(invalid_bsr).not_to be_valid
         end
