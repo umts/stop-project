cab74eba9af249e5d299fb9358746e2d427e2430
Molly O'Neil <mboneil@umass.edu>
2018-06-22 12:28:32 -0400

    Remove useless variable assignment

--- spec/models/bus_stops_route_spec.rb
+++ spec/models/bus_stops_route_spec.rb
@@ -13,7 +13,7 @@ describe BusStopsRoute do
           stop_2 = create :bus_stop
           sequence = 1
 
-          valid_bsr = create :bus_stops_route, route: route, bus_stop: stop_1, sequence: sequence, direction: direction
+          create :bus_stops_route, route: route, bus_stop: stop_1, sequence: sequence, direction: direction
           invalid_bsr = build :bus_stops_route, route: route, bus_stop: stop_2, sequence: sequence, direction: direction
 
           expect(invalid_bsr).not_to be_valid
@@ -21,7 +21,7 @@ describe BusStopsRoute do
       end
       context 'same bus_stop' do
         it 'is not valid' do
-          valid_bsr = create :bus_stops_route, route: route, bus_stop: stop_1, direction: direction
+          create :bus_stops_route, route: route, bus_stop: stop_1, direction: direction
           invalid_bsr = build :bus_stops_route, route: route, bus_stop: stop_1, direction: direction
 
           expect(invalid_bsr).not_to be_valid
