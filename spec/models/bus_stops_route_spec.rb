f2cfbee9206f11df7428066930ff4a0f344b2f5f
Molly O'Neil <mboneil@umass.edu>
2018-06-22 12:24:40 -0400

    Scenario of the same bus_stop covered in tests

--- spec/models/bus_stops_route_spec.rb
+++ spec/models/bus_stops_route_spec.rb
@@ -5,9 +5,9 @@ describe BusStopsRoute do
     context 'same route and direction' do
       let!(:route) { create :route }
       let!(:direction) { 'West' }
+      let!(:stop_1) { create :bus_stop }
       context 'same sequence' do
         it 'is not valid' do
-          stop_1 = create :bus_stop
           stop_2 = create :bus_stop
           sequence = 1
 
@@ -19,6 +19,8 @@ describe BusStopsRoute do
       end
       context 'same bus_stop' do
         it 'is not valid' do
+          valid_bsr = create :bus_stops_route, route: route, bus_stop: stop_1, direction: direction
+          invalid_bsr = build :bus_stops_route, route: route, bus_stop: stop_1, direction: direction
         end
       end
     end
