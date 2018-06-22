466b12a7a2d2a418ced64272d8f0b8a6086ae74f
Molly O'Neil <mboneil@umass.edu>
2018-06-22 12:15:28 -0400

    Better test explanations and use of factories

--- spec/models/bus_stops_route_spec.rb
+++ spec/models/bus_stops_route_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe BusStopsRoute do
   describe 'validations' do
-    context 'stop with same sequence and direction as another stop' do
+    context 'same bus stop in same route and direction' do
       it 'is not valid' do
         direction = 'West'
         sequence = 1
@@ -11,11 +11,14 @@ describe BusStopsRoute do
         bs2 = create :bus_stop
 
         # create bus_stops_route with same sequence number and direction
-        # bsr1 = create :bus_stops_route, route: route, bus_stop: bs1, sequence: sequence, direction: direction
-        # bsr2 = build :bus_stops_route, route: route, bus_stop: bs2, sequence: sequence, direction: direction
+        bsr1 = create :bus_stops_route, route: route, bus_stop: bs1, sequence: sequence, direction: direction
+        bsr2 = build :bus_stops_route, route: route, bus_stop: bs2, sequence: sequence, direction: direction
         
         expect(bsr2).not_to be_valid
-        expect(review).not_to be_valid
+      end
+    end
+    context 'same sequence in same route and direction' do
+      it 'is not valid' do
       end
     end
   end
