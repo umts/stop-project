79ab82d4dfa906902a76985bb2f9a3afe0722144
Molly O'Neil <mboneil@umass.edu>
2018-06-22 12:40:09 -0400

    No need for hardcoding attributes

--- spec/models/bus_stops_route_spec.rb
+++ spec/models/bus_stops_route_spec.rb
@@ -6,28 +6,25 @@ describe BusStopsRoute do
   describe 'validations' do
     context 'same route and direction' do
       let!(:route) { create :route }
-      let!(:direction) { 'West' }
       let!(:stop_1) { create :bus_stop }
-      let!(:valid_bsr) { create :bus_stops_route, route: route,
-                                                bus_stop: stop_1,
-                                                direction: direction }
+      let!(:valid_bsr) { create :bus_stops_route, route: route, bus_stop: stop_1 }
       context 'same sequence' do
         it 'is not valid' do
           stop_2 = create :bus_stop
 
-          invalid_bsr = build :bus_stops_route, route: route,
+          invalid_bsr = build :bus_stops_route, route: valid_bsr.route,
                                                 bus_stop: stop_2,
                                                 sequence: valid_bsr.sequence,
-                                                direction: direction
+                                                direction: valid_bsr.direction
 
           expect(invalid_bsr).not_to be_valid
         end
       end
       context 'same bus_stop' do
         it 'is not valid' do
-          invalid_bsr = build :bus_stops_route, route: route,
+          invalid_bsr = build :bus_stops_route, route: valid_bsr.route,
                                                 bus_stop: valid_bsr.bus_stop,
-                                                direction: direction
+                                                direction: valid_bsr.direction
 
           expect(invalid_bsr).not_to be_valid
         end
