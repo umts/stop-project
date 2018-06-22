40eb7d6cbbdef1711288f73def4d10d9868b4e42
Molly O'Neil <mboneil@umass.edu>
2018-06-22 12:17:33 -0400

    Rename variables to be clearer

--- spec/models/bus_stops_route_spec.rb
+++ spec/models/bus_stops_route_spec.rb
@@ -4,16 +4,16 @@ describe BusStopsRoute do
   describe 'validations' do
     context 'same bus stop in same route and direction' do
       it 'is not valid' do
+        route = create :route
+        stop_1 = create :bus_stop
+        stop_2 = create :bus_stop
         direction = 'West'
         sequence = 1
-        route = create :route
-        bs1 = create :bus_stop
-        bs2 = create :bus_stop
 
-        bsr1 = create :bus_stops_route, route: route, bus_stop: bs1, sequence: sequence, direction: direction
-        bsr2 = build :bus_stops_route, route: route, bus_stop: bs2, sequence: sequence, direction: direction
+        valid_bsr = create :bus_stops_route, route: route, bus_stop: stop_1, sequence: sequence, direction: direction
+        invalid_bsr = build :bus_stops_route, route: route, bus_stop: stop_2, sequence: sequence, direction: direction
         
-        expect(bsr2).not_to be_valid
+        expect(invalid_bsr).not_to be_valid
       end
     end
     context 'same sequence in same route and direction' do
