e693cca529cafc584cf395fd371dbbd9f372012a
Molly O'Neil <mboneil@umass.edu>
2018-06-22 12:20:00 -0400

    Abstract out similarities between tests into another context

--- spec/models/bus_stops_route_spec.rb
+++ spec/models/bus_stops_route_spec.rb
@@ -2,22 +2,24 @@ require 'spec_helper'
 
 describe BusStopsRoute do
   describe 'validations' do
-    context 'same bus stop in same route and direction' do
-      it 'is not valid' do
-        route = create :route
-        stop_1 = create :bus_stop
-        stop_2 = create :bus_stop
-        direction = 'West'
-        sequence = 1
+    context 'same route and direction' do
+      context 'same bus stop' do
+        it 'is not valid' do
+          route = create :route
+          stop_1 = create :bus_stop
+          stop_2 = create :bus_stop
+          direction = 'West'
+          sequence = 1
 
-        valid_bsr = create :bus_stops_route, route: route, bus_stop: stop_1, sequence: sequence, direction: direction
-        invalid_bsr = build :bus_stops_route, route: route, bus_stop: stop_2, sequence: sequence, direction: direction
-        
-        expect(invalid_bsr).not_to be_valid
+          valid_bsr = create :bus_stops_route, route: route, bus_stop: stop_1, sequence: sequence, direction: direction
+          invalid_bsr = build :bus_stops_route, route: route, bus_stop: stop_2, sequence: sequence, direction: direction
+          
+          expect(invalid_bsr).not_to be_valid
+        end
       end
-    end
-    context 'same sequence in same route and direction' do
-      it 'is not valid' do
+      context 'same sequence' do
+        it 'is not valid' do
+        end
       end
     end
   end
