4415f66b0572789530b97b37b3dc65713bc21323
Molly O'Neil <mboneil@umass.edu>
2018-06-22 11:57:18 -0400

    Outline for validations of bus_stops_routes

--- spec/models/bus_stops_route_spec.rb
+++ spec/models/bus_stops_route_spec.rb
@@ -1,5 +1,22 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe BusStopsRoute, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe BusStopsRoute do
+  describe 'validations' do
+    context 'stop with same sequence and direction as another stop' do
+      it 'is not valid' do
+        direction = 'West'
+        sequence = 1
+        route = create :route
+        bs1 = create :bus_stop
+        bs2 = create :bus_stop
+
+        # create bus_stops_route with same sequence number and direction
+        # bsr1 = create :bus_stops_route, route: route, bus_stop: bs1, sequence: sequence, direction: direction
+        # bsr2 = build :bus_stops_route, route: route, bus_stop: bs2, sequence: sequence, direction: direction
+        
+        expect(bsr2).not_to be_valid
+        expect(review).not_to be_valid
+      end
+    end
+  end
 end
