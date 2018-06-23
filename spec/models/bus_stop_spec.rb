4ff368274183feffebce931c5bd2ad2d514b16c1
Molly O'Neil <mboneil@umass.edu>
2018-06-22 22:52:14 -0400

    Skeleton for unit tests

--- spec/models/bus_stop_spec.rb
+++ spec/models/bus_stop_spec.rb
@@ -1,5 +1,27 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe BusStop, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe BusStop do
+  describe 'assign_completion_timestamp' do
+    context 'bus stop is completed' do
+      it 'assigns the current time to completed_at' do
+      end
+    end
+    context 'bus stop is not completed' do
+      it 'assigns nil to completed at' do
+      end
+    end
+  end
+
+  describe 'decide_if_completed_by' do
+    context 'bus stop completed attribute changed' do
+      context 'bus stop is completed' do
+        it 'assigns user to completed by' do
+        end
+      end
+      context 'bus stop is not completed' do
+        it 'assigns nil to completed by' do
+        end
+      end
+    end
+  end
 end
