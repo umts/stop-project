0cfcf6059a29d6bd87a099212f46a70f08425968
Molly O'Neil <mboneil@umass.edu>
2018-06-24 17:13:32 -0400

    Messed up the field guide test, fixed now

--- spec/system/bus_stops/editing_bus_stops_spec.rb
+++ spec/system/bus_stops/editing_bus_stops_spec.rb
@@ -6,7 +6,7 @@ describe 'editing a bus stop as a user' do
   let!(:edit_stop) { create :bus_stop }
   before :each do
     when_current_user_is user
-    visit bus_stop_path(edit_stop)
+    visit edit_bus_stop_path(edit_stop.hastus_id)
   end
   context 'with no changes' do
     before :each do
@@ -45,20 +45,20 @@ describe 'editing a bus stop as a user' do
     end
     it 'sends a helpful error message' do
       expect(page).to have_selector 'p.errors',
-                                    text: "Can't be blank"
+                                    text: "Bench can't be blank"
     end
     it 'stays on the edit page' do
-      expect(page).to have_current_path(bus_stop_path(id: edit_stop))
+      expect(page).to have_current_path(edit_bus_stop_path(edit_stop.hastus_id))
     end
   end
   context 'clicking on the field guide link' do
     before :each do
       within 'table.edit-form' do
-        click 'Field Guide'
+        click_on 'Field Guide'
       end
     end
     it 'redirects to the field guide' do
-      expect(page).to have_current_path(field_guide_bus_stop_path)
+      expect(page.current_url).to end_with field_guide_bus_stops_path
     end
   end
 end
