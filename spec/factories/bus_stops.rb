d1859fa58755280af8dc37c95f50d7530495ac6c
Molly O'Neil <mboneil@umass.edu>
2018-06-24 13:35:50 -0400

    Assigning these attrs in factory bot is too much

--- spec/factories/bus_stops.rb
+++ spec/factories/bus_stops.rb
@@ -4,5 +4,44 @@ FactoryBot.define do
   factory :bus_stop do
     name 'stop'
     sequence :hastus_id
+    trait :completed do
+      name 'shit'
+      sequence :hastus_id
+      bench 'PVTA'
+      curb_cut 'ok'
+      lighting 'solar'
+      mounting 'hi'
+      mounting_direction 'shit'
+      schedule_holder 'shit'
+      shelter 'shit'
+      sidewalk_width 'shit'
+      trash 'to'
+      mounting_clearance 'yeah'
+      created_at Date.yesterday
+      updated_at Date.today
+      sign_type 'shit'
+      shelter_condition 'good'
+      shelter_pad_condition 'ugh'
+      shelter_pad_material 'shit'
+      shelter_type 'shit'
+      shared_sign_post 'shit'
+      garage_responsible 'UMTs'
+      bike_rack 'shit'
+      real_time_information 'shit'
+      need_work 'shit'
+      obstructions 'shit'
+      stop_sticker 'shit'
+      route_stickers 'shits'
+      bolt_on_base true
+      bus_pull_out_exists true
+      has_power true
+      solar_lighting true
+      system_map_exists true
+      shelter_ada_compliance true
+      ada_landing_pad true
+      state_road true
+      accessible true
+      completed true
+    end
   end
 end
