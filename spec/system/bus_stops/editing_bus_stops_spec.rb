fcc25d3b18103f16eaa1889e7032b496f5b66e7e
Molly O'Neil <mboneil@umass.edu>
2018-06-24 18:01:04 -0400

    Another test for completed label

--- spec/system/bus_stops/editing_bus_stops_spec.rb
+++ spec/system/bus_stops/editing_bus_stops_spec.rb
@@ -80,4 +80,24 @@ describe 'editing a bus stop as a user' do
         expect(page).to have_content "Last updated by #{last_user.name} at #{format_datetime(1.day.ago)}"
     end
   end
+  # same issue as above context with check 'Completed'
+  context 'with a bus stop that has been completed' do
+    let!(:last_user) { create :user }
+    let!(:user) { create :user }
+    let!(:edit_stop) { create :bus_stop, :pending }
+    it 'displays who updated it and when' do
+        Timecop.freeze 1.day.ago do
+          when_current_user_is last_user
+          visit edit_bus_stop_path(edit_stop.hastus_id)
+          within 'table.edit-form' do
+            select 'Yes', from: 'Accessible'
+            check 'Completed'
+            click_button 'Save stop'
+          end
+        end
+        when_current_user_is user
+        visit edit_bus_stop_path(edit_stop.hastus_id)
+        expect(page).to have_content "Completed by #{last_user.name} at #{format_datetime(1.day.ago)}"
+    end
+  end
 end
