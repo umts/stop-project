df91ebc2513ff6f6e780f4a7c4994ead838cc7f5
Molly O'Neil <mboneil@umass.edu>
2018-06-29 15:32:49 -0400

    Declare attributes outright

--- spec/factories/bus_stops.rb
+++ spec/factories/bus_stops.rb
@@ -5,20 +5,40 @@ FactoryBot.define do
     name 'stop'
     sequence :hastus_id
     trait :pending do
-      %i[name bench curb_cut lighting mounting mounting_direction
-        schedule_holder shelter sidewalk_width trash mounting_clearance
-        sign_type shelter_condition shelter_pad_condition shelter_pad_material
-        shelter_type shared_sign_post garage_responsible need_work obstructions
-        stop_sticker route_stickers bike_rack real_time_information].each do |s|
-        send(s, 'not blank')
-        end
+      name 'not blank'
+      bench
+      curb_cut
+      lighting
+      mounting
+      mounting_direction
+      schedule_holder
+      shelter
+      sidewalk_width
+      trash
+      mounting_clearance
+      sign_type
+      shelter_condition
+      shelter_pad_condition
+      shelter_pad_material
+      shelter_type
+      shared_sign_post
+      garage_responsible
+      need_work
+      obstructions
+      stop_sticker
+      route_stickers
+      bike_rack
+      real_time_information
       created_at Date.yesterday
       updated_at Date.today
-      %i[bolt_on_base bus_pull_out_exists has_power solar_lighting
-        system_map_exists shelter_ada_compliance ada_landing_pad
-        state_road].each do |b|
-        send(b, 'true')
-        end
+      bolt_on_base
+      bus_pull_out_exists
+      has_power
+      solar_lighting
+      system_map_exists
+      shelter_ada_compliance
+      ada_landing_pad
+      state_road
     # all attrs except for accessible
     end
     trait :completed do
