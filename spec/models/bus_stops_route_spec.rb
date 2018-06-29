576daf9b85d47a6eb8da576e8ff029b18ffdc99b
Molly O'Neil <mboneil@umass.edu>
2018-06-29 13:33:32 -0400

    Rubocop fixes

--- spec/models/bus_stops_route_spec.rb
+++ spec/models/bus_stops_route_spec.rb
@@ -7,11 +7,11 @@ describe BusStopsRoute do
     context 'same route and direction' do
       let(:route) { create :route }
       let(:stop_1) { create :bus_stop }
-      let(:valid_bsr) {
+      let(:valid_bsr) do
         create :bus_stops_route,
                route: route,
                bus_stop: stop_1
-      }
+      end
       context 'same sequence' do
         it 'is not valid' do
           stop_2 = create :bus_stop
@@ -36,11 +36,11 @@ describe BusStopsRoute do
     end
   end
   describe 'import' do
-    let(:main_variant) { ['A', 'B', 'C', 'D', 'E'] }
-    let(:route_dir) { ['31', 'North'] }
+    let(:main_variant) { %w[A B C D E] }
+    let(:route_dir) { %w[31 North] }
     context 'all stops are in longest variant' do
       it 'sequences stops correctly' do
-        stop_hash = { route_dir => {'m_North' => main_variant } }
+        stop_hash = { route_dir => { 'm_North' => main_variant } }
         BusStopsRoute.import(stop_hash)
         expect(stop_hash[route_dir]).to be main_variant
       end
@@ -48,12 +48,10 @@ describe BusStopsRoute do
     context 'stop (but not first stop) is not in longest variant' do
       context 'one stop is in other variant' do
         it 'sequences stops correctly' do
-          sequenced_stops = ['A', 'F', 'B', 'C', 'D', 'E']
+          sequenced_stops = %w[A F B C D E]
           stop_hash = { route_dir =>
                         { 'm_North' => main_variant,
-                          'o_North' => ['A', 'F', 'E']
-                        }
-                      }
+                          'o_North' => %w[A F E] } }
           BusStopsRoute.import(stop_hash)
           expect(stop_hash[route_dir]).to eql sequenced_stops
         end
