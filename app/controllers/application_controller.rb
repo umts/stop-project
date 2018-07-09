acee0be039a0bf4407d4258d1221bf0fdc97dde9
Justin <juforgue@gmail.com>
2018-07-09 11:37:05 -0400

    Add before action to store user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :set_paper_trail_whodunnit
+  before_action :prepare_exception_notifier
 
   helper_method :format_datetime
   
   private
 
+  def prepare_exception_notifier
+    request.env["exception_notifier.exception_data"] = {
+        :current_user => current_user
+    }
+  end
   def restrict_to_admin
     head :unauthorized and return unless current_user.admin?
   end
