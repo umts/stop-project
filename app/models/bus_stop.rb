9d790c62f9ec7d58c5186fe1a3d9dbd21218b3e3
Molly O'Neil <mboneil@umass.edu>
2018-07-11 14:32:43 -0400

    data_fields method back to the way it was

--- app/models/bus_stop.rb
+++ app/models/bus_stop.rb
@@ -23,8 +23,7 @@ class BusStop < ApplicationRecord
   def data_fields
     Field.categories.map do |category|
       category_fields = Field.in_category(category).order(:rank).map do |field|
-        # need to find last version of BusStopField or create it.
-        BusStopField.where(field: field, bus_stop: self).order(:created_at).last rescue BusStopField.create(field: field, bus_stop: self)
+        BusStopField.where(field: field, bus_stop: self).first_or_create
       end
       [category, category_fields]
     end.to_h
