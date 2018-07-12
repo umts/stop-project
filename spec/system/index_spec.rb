d14ef756ee363b9c152ca41bb86aa751b6262105
Molly O'Neil <mboneil@umass.edu>
2018-07-12 10:48:57 -0400

    Visit the by sequence and by status pages properly

--- spec/system/index_spec.rb
+++ spec/system/index_spec.rb
@@ -117,7 +117,7 @@ describe 'searching for a bus stop by route' do
   context 'incorrect route in params' do
     context 'viewing stops by status' do
       before :each do
-        visit by_status_bus_stops_path(incorrect_route_number)
+        visit by_status_bus_stops_url(number: incorrect_route_number)
       end
       it 'redirects to index page' do
         expect(page.current_path).to end_with bus_stops_path
@@ -129,7 +129,7 @@ describe 'searching for a bus stop by route' do
     end
     context 'viewing stops by sequence' do
       before :each do
-        visit by_sequence_bus_stops_path(incorrect_route_number)
+        visit by_sequence_bus_stops_url(number: incorrect_route_number)
       end
       it 'redirects to index page' do
         expect(page.current_path).to end_with bus_stops_path
