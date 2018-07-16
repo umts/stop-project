4020b0f60032919cacdacb981b805c68a7bb269c
Molly O'Neil <mboneil@umass.edu>
2018-07-16 12:27:43 -0400

    Strip tests and add in describe, contexts for a controller test

--- spec/controllers/bus_stops_controller_spec.rb
+++ spec/controllers/bus_stops_controller_spec.rb
@@ -2,29 +2,19 @@ require 'spec_helper'
 
 describe BusStopsController, type: :controller do
   let(:incorrect_route_number) { '-1' }
-  context 'incorrect route in params' do
-    context 'viewing stops by status' do
-      before :each do
-        visit by_status_bus_stops_url(number: incorrect_route_number)
-      end
+  describe 'by_status' do
+    context 'incorrect route in params' do
       it 'redirects to index page' do
-        expect(page.current_path).to end_with bus_stops_path
       end
       it 'displays a helpful message' do
-        expect(page).to have_selector 'p.notice',
-                                      text: 'Route -1 not found'
       end
     end
-    context 'viewing stops by sequence' do
-      before :each do
-        visit by_sequence_bus_stops_url(number: incorrect_route_number)
-      end
+  end
+  describe 'by_sequence' do
+    context 'incorrect route in params' do
       it 'redirects to index page' do
-        expect(page.current_path).to end_with bus_stops_path
       end
       it 'displays a helpful message' do
-        expect(page).to have_selector 'p.notice',
-                                      text: 'Route -1 not found'
       end
     end
   end
