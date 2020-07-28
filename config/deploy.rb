917c58821d67861615b8d18ec7da53871e3199bb
Matt Moretti <werebus@gmail.com>
2020-07-28 17:46:17 -0400

    Upgraded capistrano/capistrano-bundler
    Also Bundler version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,11 @@
-# config valid only for current version of Capistrano
-lock '3.8.1'
+lock '~> 3.14.1'
 
 set :application, 'stop-project'
 set :repo_url, 'https://github.com/umts/stop-project.git'
 set :branch, :master
 set :deploy_to, "/srv/#{fetch :application}"
-set :log_level, :info
-set :keep_releases, 5
 
-set :linked_files, fetch(:linked_files, []).push('config/database.yml')
-set :linked_dirs, fetch(:linked_dirs, []).push('log')
+set :log_level, :info
+
+append :linked_files, 'config/database.yml'
+append :linked_dirs, '.bundle', 'log'
