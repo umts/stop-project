c3bd56dc61b514479ace4d357232de88d1ec526d
Matt Moretti <werebus@gmail.com>
2020-12-23 17:27:21 -0500

    Toggle Rails 5.2 defaults on

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   include DateAndTimeMethods
 
-  protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :set_paper_trail_whodunnit
   before_action :prepare_exception_notifier
