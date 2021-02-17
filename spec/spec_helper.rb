f4e1dff35cb2996889720f5030ed1f03660e3d1f
Matt Moretti <werebus@gmail.com>
2021-02-17 16:51:14 -0500

    Setup driven_by

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,6 +39,14 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+
+  config.before :each, type: :system do
+    driven_by :rack_test
+  end
+
+  config.before :each, type: :system, js: true do
+    driven_by :selenium, using: :headless_chrome
+  end
 end
 
 def when_current_user_is(user)
