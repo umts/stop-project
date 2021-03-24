7cb53764f1198288f53857bc022c3782a8ad1b3e
Matt Moretti <werebus@gmail.com>
2021-03-24 14:49:05 -0400

    Load 6.0 defaults

--- config/application.rb
+++ config/application.rb
@@ -22,14 +22,12 @@ Bundler.require(*Rails.groups)
 module StopProject
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.encoding = 'utf-8'
     config.time_zone = 'Eastern Time (US & Canada)'
-    config.filter_parameters += [:password, :secret]
   end
 end
