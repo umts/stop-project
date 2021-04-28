ac504030f3613c1c15383cc2d213aa15e38aa626
Matt Moretti <werebus@gmail.com>
2021-04-28 12:13:07 -0400

    Rubocopped controller specs

--- spec/controllers/bus_stops_controller_spec.rb
+++ spec/controllers/bus_stops_controller_spec.rb
@@ -47,31 +47,24 @@ describe BusStopsController do
   end
 
   describe 'update' do
+    subject(:submit) do
+      put :update, params: { id: bsr1.bus_stop.hastus_id,
+                             bus_stop: { name: bsr1.bus_stop.name },
+                             commit: 'Save and next',
+                             route_id: route.id,
+                             direction: bsr1.direction }
+    end
+
     let(:route) { create :route }
-    let(:bus_stop1) { create :bus_stop }
-    let(:bus_stop2) { create :bus_stop }
-    let!(:bus_stops_route1) do
-      create :bus_stops_route, route: route, bus_stop: bus_stop1
-    end
-    let!(:bus_stops_route2) do
-      create :bus_stops_route, route: route, bus_stop: bus_stop2
-    end
+    let!(:bsr1) { create :bus_stops_route, route: route }
+    let!(:bsr2) { create :bus_stops_route, route: route }
 
-    context 'when updating a sequence of routes' do
-      before do
-        put :update, params: { id: bus_stop1.hastus_id,
-                               bus_stop: { name: bus_stop1.name },
-                               commit: 'Save and next',
-                               route_id: route.id,
-                               direction: bus_stops_route1.direction }
-      end
-
-      it 'redirects to edit next stop' do
-        path = edit_bus_stop_path id: bus_stop2.hastus_id,
-                                  direction: bus_stops_route2.direction,
-                                  route_id: route.id
-        expect(response).to redirect_to(path)
-      end
+    it 'redirects to edit next stop when specifying a route and direction' do
+      path = edit_bus_stop_path id: bsr2.bus_stop.hastus_id,
+                                direction: bsr2.direction,
+                                route_id: route.id
+      submit
+      expect(response).to redirect_to(path)
     end
   end
 end
