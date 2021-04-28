50359e477a3bff038f07ae0c757f750505cbacfa
Matt Moretti <werebus@gmail.com>
2021-04-28 16:54:36 -0400

    Added strong password gem

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'mysql2', '~> 0.4'
 gem 'paper_trail', '~> 11.1'
 gem 'rails', '~> 6.1.3'
 gem 'sassc-rails'
+gem 'strong_password'
 gem 'underscore-rails', '~> 1.8'
 gem 'will_paginate', '~> 3.1'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :validatable
+
   validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, password_strength: true, if: :password_required?
+
   has_many :stops_completed, class_name: 'BusStop', foreign_key: 'completed_by'
 
   def not_admin?
