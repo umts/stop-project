154b8c45c89efd83b58a8e03241f2b6f38a0ad89
Matt Moretti <werebus@gmail.com>
2023-10-13 12:05:43 -0400

    Add turbo-rails

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@rails/ujs"
 import "controllers"
+import "@hotwired/turbo-rails"
--- config/importmap.rb
+++ config/importmap.rb
@@ -4,5 +4,6 @@ pin 'application', preload: true
 pin '@rails/ujs', to: 'https://ga.jspm.io/npm:@rails/ujs@7.1.0/app/assets/javascripts/rails-ujs.esm.js'
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
+pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin 'stimulus-autocomplete', to: 'https://ga.jspm.io/npm:stimulus-autocomplete@3.1.0/src/autocomplete.js'
 pin_all_from 'app/javascript/controllers', under: 'controllers'
