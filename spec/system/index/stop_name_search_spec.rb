e7bb2f12aaa56a96793ee59c4307371641b1e147
Matt Moretti <werebus@gmail.com>
2023-10-13 11:27:49 -0400

    Fix specs

--- spec/system/index/stop_name_search_spec.rb
+++ spec/system/index/stop_name_search_spec.rb
@@ -14,8 +14,8 @@ RSpec.describe 'searching for a bus stop by stop name' do
 
   context 'with a correct stop name' do
     it 'redirects to the edit page' do
-      within 'form', text: 'Enter stop name' do
-        fill_in 'Enter stop name', with: bus_stop.name
+      within 'form', text: 'Enter Stop Name' do
+        fill_in 'Enter Stop Name', with: bus_stop.name
         click_button 'Search'
       end
       expect(page).to have_content "Editing #{bus_stop.name}"
@@ -24,28 +24,26 @@ RSpec.describe 'searching for a bus stop by stop name' do
 
   context 'with an incorrect stop name' do
     before do
-      within 'form', text: 'Enter stop name' do
-        fill_in 'Enter stop name', with: incorrect_stop_name
+      within 'form', text: 'Enter Stop Name' do
+        fill_in 'Enter Stop Name', with: incorrect_stop_name
         click_button 'Search'
       end
     end
 
     it 'stays on the same page' do
-      expect(page).to have_current_path bus_stops_path
+      expect(page).to have_current_path root_path
     end
 
     it 'displays a helpful message' do
-      expect(page).to have_selector 'p.notice',
-                                    text: "Stop #{incorrect_stop_name} not found"
+      expect(page).to have_selector 'p.notice', text: "Stop #{incorrect_stop_name} not found"
     end
   end
 
   context 'without completing stop name' do
-    it 'autofills', js: true do
-      # use the first character of the stop name so it's incomplete,
-      # in order to be completed by autocomplete
-      fill_in 'Enter stop name', with: bus_stop.name.chars.first
-      find('div', text: bus_stop.name).click
+    it 'autocompletes', js: true do
+      find '[data-autocomplete-ready-value=true]'
+      fill_in 'Enter Stop Name', with: bus_stop.name.first
+      click_on bus_stop.name
       expect(page).to have_content "Editing #{bus_stop.name}"
     end
   end
