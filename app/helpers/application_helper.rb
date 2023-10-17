2c2971368ca9338df273e205edd0987318e8d0d2
Matt Moretti <werebus@gmail.com>
2023-10-17 16:44:38 -0400

    Remove `display_name` helper
    It does way too many jobs, but is only used for one of those jobs.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,4 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def display_name(item)
-    case item
-    when User
-      item.name || 'New user'
-    when BusStop
-      item.name || 'New stop'
-    end
-  end
 end
