53a657d9608476f316f241c0879a9e8c14791a22
Matt Moretti <werebus@gmail.com>
2023-10-18 09:28:18 -0400

    Pare down navbar customization
    * Gid rid of colors, we can put them back via the better Sass variables
      in 4
    * Stop having buttons _in_ links
    * Added handling for the current page
    
    At some point it might be nice to get the collapse working, but we don't
    have any of Bootstrap's JS currently.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,10 @@ module ApplicationHelper
   def yes_no(value)
     tag.span (value ? 'Yes' : 'No'), class: (value ? 'text-success' : 'text-danger')
   end
+
+  def nav_item(name, options = nil, html_options = nil, &block)
+    tag.li class: (current_page?(options) ? 'active' : nil) do
+      link_to name, options, html_options, &block
+    end
+  end
 end
