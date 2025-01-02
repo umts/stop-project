642869bb3d87a5574999547d63777b0c44a51312
Matt Moretti <werebus@gmail.com>
2025-01-02 17:27:33 -0500

    Don't use the system libxml2
    Who knows why this was added (it appears to have been copied over from
    another app). But, it hsan't _done_ anything as long as nokogiri has
    had precompiled gems available.
    
    After #363, though, Bundler is now compiling the gem natively. And, it
    turns out the the RHEL-provided version of libxml2 is too old. Luckily,
    nokogiri ships with a vendored copy of that C library. We just have to
    not not use it.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,5 +4,4 @@ server 'af-transit-app3.admin.umass.edu',
        roles: %w[app db web],
        ssh_options: { forward_agent: false }
 
-set :bundle_env_variables, { 'NOKOGIRI_USE_SYSTEM_LIBRARIES' => 1 }
 set :default_env, { 'SECRET_KEY_BASE' => 'NOT_A_REAL_SECRET_AND_THATS_OK' }
